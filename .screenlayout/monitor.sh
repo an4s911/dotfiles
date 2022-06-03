d624a115298a1d730daf8567cf1ac97ec6bba429
Anas Basheer <nasbas23@gmail.com>
2022-06-03 19:40:21 +0300

    Backup at 03 Jun 2022 19:40

--- .screenlayout/monitor.sh
+++ .screenlayout/monitor.sh
@@ -3,5 +3,5 @@
 if [ "$(xrandr | grep "^DP1 connected")" ]; then
     xrandr --output DP1 --primary --mode 1366x768 --output eDP1 --mode 1366x768 --right-of DP1
 else
-    xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0
+    xrandr --output eDP1 --primary --mode 1366x768 --output DP1 --off
 fi
